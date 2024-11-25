642f1fefa6c59ad5be61c7a3c3aed500475fcad2
1zuna <1zuna@ccbluex.net>
2024-11-25 17:41:03 +0100

    fix(FakeLag): concurrent modification error (#4678)

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleFakeLag.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleFakeLag.kt
@@ -62,6 +62,8 @@ object ModuleFakeLag : Module("FakeLag", Category.COMBAT) {
 
     private var nextDelay = delay.random()
 
+    private var isEnemyNearby = false
+
     fun shouldLag(packet: Packet<*>?): Boolean {
         if (!enabled || !inGame || player.isDead || player.isTouchingWater || mc.currentScreen != null) {
             return false
@@ -114,7 +116,9 @@ object ModuleFakeLag : Module("FakeLag", Category.COMBAT) {
             Mode.CONSTANT -> true
             Mode.DYNAMIC -> {
                 // If there is an enemy in range, we want to lag.
-                world.findEnemy(range) ?: return false
+                if (!isEnemyNearby) {
+                    return false
+                }
 
                 val (playerPosition, _, _) = FakeLag.firstPosition() ?: return true
                 val playerBox = player.dimensions.getBoxAt(playerPosition)
@@ -147,7 +151,10 @@ object ModuleFakeLag : Module("FakeLag", Category.COMBAT) {
         }
     }
 
-    val repeatable = repeatable {
+    @Suppress("unused")
+    private val gameTickHandler = repeatable {
+        isEnemyNearby = world.findEnemy(range) != null
+
         if (evadeArrows) {
             val (playerPosition, _, _) = FakeLag.firstPosition() ?: return@repeatable
 
@@ -171,6 +178,11 @@ object ModuleFakeLag : Module("FakeLag", Category.COMBAT) {
                 FakeLag.flush(evadingPacket.idx + 1)
             }
         }
+    }
+
+    override fun disable() {
+        isEnemyNearby = false
+        super.disable()
     }
 
 }

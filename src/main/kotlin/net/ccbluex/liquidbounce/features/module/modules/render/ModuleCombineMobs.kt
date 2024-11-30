7e0d518018de8085318c59476443aeb43e157513
MukjepScarlet <93977077+MukjepScarlet@users.noreply.github.com>
2024-12-01 06:35:01 +0800

    refactor(CombineMobs): save mob count of positions (#4692)

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleCombineMobs.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleCombineMobs.kt
@@ -18,6 +18,7 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.render
 
+import it.unimi.dsi.fastutil.longs.Long2IntOpenHashMap
 import net.ccbluex.liquidbounce.event.events.GameRenderEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
@@ -25,7 +26,6 @@ import net.ccbluex.liquidbounce.features.module.ClientModule
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.mob.MobEntity
-import net.minecraft.util.math.BlockPos
 
 /**
  * Combine Mobs
@@ -39,7 +39,10 @@ import net.minecraft.util.math.BlockPos
  */
 object ModuleCombineMobs : ClientModule("CombineMobs", Category.RENDER) {
 
-    private val trackedEntitySinceRender = mutableMapOf<EntityType<*>, MutableList<BlockPos>>()
+    /**
+     * Key: type Value: position->count
+     */
+    private val trackedEntitySinceRender = hashMapOf<EntityType<*>, Long2IntOpenHashMap>()
 
     /**
      * As soon we disable the module, we want to clear the tracked entities
@@ -57,20 +60,19 @@ object ModuleCombineMobs : ClientModule("CombineMobs", Category.RENDER) {
     }
 
     fun trackEntity(entity: Entity): Boolean {
-        val entityType = entity.type
-
         if (entity !is MobEntity) {
             return false
         }
 
-        val pos = entity.blockPos
+        val entityType = entity.type
 
-        val trackedEntities = trackedEntitySinceRender.getOrPut(entityType) { mutableListOf() }
-        val countOf = trackedEntities.count { it == pos }
-        trackedEntities += pos
-        trackedEntitySinceRender[entityType] = trackedEntities
+        val pos = entity.blockPos.asLong()
 
-        return countOf > 0
+        val trackedEntities = trackedEntitySinceRender.getOrPut(entityType, ::Long2IntOpenHashMap)
+        val count = trackedEntities.getOrDefault(pos, 0)
+        trackedEntities.put(pos, count + 1)
+
+        return count > 0
     }
 
 }

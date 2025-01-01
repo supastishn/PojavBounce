9248a7f8597e24d41d5ce54752d680053e3b0e88
ccetl <109693935+ccetl@users.noreply.github.com>
2025-01-01 18:02:43 +0100

    chore(Copyright): 2025 (#5141)

--- src/main/kotlin/net/ccbluex/liquidbounce/features/misc/proxy/ProxyValidator.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/misc/proxy/ProxyValidator.kt
@@ -34,7 +34,7 @@ import kotlin.jvm.optionals.getOrNull
 /*
  * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
  *
- * Copyright (c) 2024 CCBlueX
+ * Copyright (c) 2015 - 2025 CCBlueX
  *
  * LiquidBounce is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

0ed9cedd0e8e604c628d178adcda5984a140affd
Izuna <1zuna@ccbluex.net>
2025-01-06 20:57:53 +0100

    fix(Lithium): injection point failure (#5210)

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/lithium/MixinChunkAwareBlockCollisionSweeper.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/lithium/MixinChunkAwareBlockCollisionSweeper.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Pseudo
-@Mixin(value = ChunkAwareBlockCollisionSweeper.class, remap = false)
+@Mixin(value = ChunkAwareBlockCollisionSweeper.class)
 public class MixinChunkAwareBlockCollisionSweeper {
 
     @Shadow

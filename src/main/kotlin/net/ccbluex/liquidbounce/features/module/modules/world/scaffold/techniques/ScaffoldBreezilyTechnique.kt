adba9304854ce05155de2aaa3dd14451b7784a73
Izuna <1zuna@ccbluex.net>
2025-01-07 18:38:59 +0100

    fix(Scaffold): breezily rotation inconsistency (#5216)

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/scaffold/techniques/ScaffoldBreezilyTechnique.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/scaffold/techniques/ScaffoldBreezilyTechnique.kt
@@ -21,10 +21,13 @@ package net.ccbluex.liquidbounce.features.module.modules.world.scaffold.techniqu
 import net.ccbluex.liquidbounce.event.events.MovementInputEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.modules.world.scaffold.ModuleScaffold.getTargetedPosition
+import net.ccbluex.liquidbounce.features.module.modules.world.scaffold.ModuleScaffold.rawInput
 import net.ccbluex.liquidbounce.features.module.modules.world.scaffold.techniques.ScaffoldNormalTechnique.NORMAL_INVESTIGATION_OFFSETS
 import net.ccbluex.liquidbounce.utils.aiming.Rotation
 import net.ccbluex.liquidbounce.utils.block.getState
 import net.ccbluex.liquidbounce.utils.block.targetfinding.*
+import net.ccbluex.liquidbounce.utils.entity.getMovementDirectionOfInput
+import net.ccbluex.liquidbounce.utils.kotlin.EventPriorityConvention
 import net.ccbluex.liquidbounce.utils.kotlin.random
 import net.ccbluex.liquidbounce.utils.math.geometry.Line
 import net.ccbluex.liquidbounce.utils.math.toBlockPos
@@ -34,6 +37,7 @@ import net.minecraft.item.ItemStack
 import net.minecraft.util.math.Direction
 import net.minecraft.util.math.Vec3d
 import kotlin.math.floor
+import kotlin.math.round
 
 object ScaffoldBreezilyTechnique : ScaffoldTechnique("Breezily") {
 
@@ -65,7 +69,9 @@ object ScaffoldBreezilyTechnique : ScaffoldTechnique("Breezily") {
     }
 
     @Suppress("unused")
-    private val handleMovementInput = handler<MovementInputEvent> { event ->
+    private val handleMovementInput = handler<MovementInputEvent>(
+        priority = EventPriorityConvention.SAFETY_FEATURE
+    ) { event ->
         if (!event.directionalInput.forwards || player.isSneaking) {
             return@handler
         }
@@ -120,7 +126,37 @@ object ScaffoldBreezilyTechnique : ScaffoldTechnique("Breezily") {
     }
 
     override fun getRotations(target: BlockPlacementTarget?): Rotation? {
-        return ScaffoldGodBridgeTechnique.getRotations(target)
+        if (rawInput == DirectionalInput.NONE) {
+            target ?: return null
+
+            return getRotationForNoInput(target)
+        }
+
+        val direction = getMovementDirectionOfInput(player.yaw, rawInput) + 180
+
+        // Round to 45°-steps (NORTH, NORTH_EAST, etc.)
+        val movingYaw = round(direction / 45) * 45
+        val isMovingStraight = movingYaw % 90 == 0f
+
+        return if (isMovingStraight) {
+            getRotationForStraightInput(movingYaw)
+        } else {
+            getRotationForDiagonalInput(movingYaw)
+        }
+
+    }
+
+    private fun getRotationForStraightInput(movingYaw: Float) = Rotation(movingYaw, 80f)
+
+    private fun getRotationForDiagonalInput(movingYaw: Float) = Rotation(movingYaw, 75.6f)
+
+    private fun getRotationForNoInput(target: BlockPlacementTarget): Rotation {
+        val axisMovement = floor(target.rotation.yaw / 90) * 90
+
+        val yaw = axisMovement + 45
+        val pitch = 75f
+
+        return Rotation(yaw, pitch)
     }
 
 }

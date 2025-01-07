4c9b23603528d4911374d02388b90041f4c3abc4
Izuna <1zuna@ccbluex.net>
2025-01-07 17:51:51 +0100

    fix(RenderedEntities): null on not in-game (#5214)

--- src/main/kotlin/net/ccbluex/liquidbounce/utils/entity/RenderedEntities.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/utils/entity/RenderedEntities.kt
@@ -1,9 +1,11 @@
 package net.ccbluex.liquidbounce.utils.entity
 
 import net.ccbluex.liquidbounce.event.EventListener
+import net.ccbluex.liquidbounce.event.events.DisconnectEvent
 import net.ccbluex.liquidbounce.event.events.GameTickEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.MinecraftShortcuts
+import net.ccbluex.liquidbounce.utils.client.inGame
 import net.ccbluex.liquidbounce.utils.combat.shouldBeShown
 import net.ccbluex.liquidbounce.utils.kotlin.EventPriorityConvention
 import net.minecraft.entity.LivingEntity
@@ -25,11 +27,21 @@ object RenderedEntities : Iterable<LivingEntity>, EventListener, MinecraftShortc
         registry.remove(subscriber)
     }
 
+    @Suppress("unused")
     private val tickHandler = handler<GameTickEvent>(priority = EventPriorityConvention.FIRST_PRIORITY) {
+        if (!inGame) {
+            return@handler
+        }
+
         @Suppress("UNCHECKED_CAST")
         entities = world.entities.filter {
             it is LivingEntity && it.shouldBeShown()
         } as Iterable<LivingEntity>
+    }
+
+    @Suppress("unused")
+    private val disconnectHandler = handler<DisconnectEvent> {
+        registry.clear()
     }
 
     override fun iterator(): Iterator<LivingEntity> = entities.iterator()

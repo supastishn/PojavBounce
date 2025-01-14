e97e17e98933aa009f1cd5c9da5364b57cbacd4b
MukjepScarlet <93977077+MukjepScarlet@users.noreply.github.com>
2025-01-14 21:10:52 +0800

    refactor: improve levenshtein calculation (#5312)

--- src/main/kotlin/net/ccbluex/liquidbounce/utils/math/StringDistance.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/utils/math/StringDistance.kt
@@ -18,32 +18,32 @@
  */
 package net.ccbluex.liquidbounce.utils.math
 
-import kotlin.math.min
-
 fun levenshtein(lhs: CharSequence, rhs: CharSequence): Int {
     val lhsLength = lhs.length
     val rhsLength = rhs.length
 
-    var cost = Array(lhsLength) { it }
-    var newCost = Array(lhsLength) { 0 }
+    if (lhsLength == 0) return rhsLength
+    if (rhsLength == 0) return lhsLength
 
-    for (i in 1 until rhsLength) {
-        newCost[0] = i
+    // DP
+    val cost = IntArray(lhsLength + 1) { it }
 
-        for (j in 1 until lhsLength) {
+    for (i in 1..rhsLength) {
+        var prevCost = cost[0]
+        cost[0] = i
+
+        for (j in 1..lhsLength) {
+            val currentCost = cost[j]
             val match = if (lhs[j - 1] == rhs[i - 1]) 0 else 1
 
-            val costReplace = cost[j - 1] + match
-            val costInsert = cost[j] + 1
-            val costDelete = newCost[j - 1] + 1
-
-            newCost[j] = min(min(costInsert, costDelete), costReplace)
+            cost[j] = minOf(
+                cost[j] + 1,
+                cost[j - 1] + 1,
+                prevCost + match
+            )
+            prevCost = currentCost
         }
-
-        val swap = cost
-        cost = newCost
-        newCost = swap
     }
 
-    return cost[lhsLength - 1]
+    return cost[lhsLength]
 }

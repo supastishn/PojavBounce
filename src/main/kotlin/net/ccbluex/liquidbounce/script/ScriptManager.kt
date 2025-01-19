7a2336684cc393974d6e93e09a8a4ef24f1e964e
Izuna <1zuna@ccbluex.net>
2025-01-19 10:08:14 +0100

    fix(ScriptManager): not reloading ClickGUI cache (#5364)
    fixes https://github.com/CCBlueX/LiquidBounce/issues/5228

--- src/main/kotlin/net/ccbluex/liquidbounce/script/ScriptManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/script/ScriptManager.kt
@@ -18,7 +18,9 @@
  */
 package net.ccbluex.liquidbounce.script
 
+import com.mojang.blaze3d.systems.RenderSystem
 import net.ccbluex.liquidbounce.config.ConfigSystem
+import net.ccbluex.liquidbounce.features.module.modules.render.ModuleClickGui
 import net.ccbluex.liquidbounce.utils.client.logger
 import org.graalvm.polyglot.Engine
 import org.graalvm.polyglot.Source
@@ -146,6 +148,11 @@ object ScriptManager {
      */
     fun enableAll() {
         scripts.forEach(PolyglotScript::enable)
+
+        // Reload the ClickGUI to update the module list.
+        RenderSystem.recordRenderCall {
+            ModuleClickGui.reloadView()
+        }
     }
 
     /**
@@ -160,7 +167,15 @@ object ScriptManager {
      * directory, and then enables them. It logs a message upon successful completion.
      */
     fun reload() {
-        unloadAll()
+        // Unload
+        try {
+            disableAll()
+            unloadAll()
+        } catch (e: Exception) {
+            logger.error("Failed to unload scripts.", e)
+        }
+
+        // Load
         loadAll()
         enableAll()
 

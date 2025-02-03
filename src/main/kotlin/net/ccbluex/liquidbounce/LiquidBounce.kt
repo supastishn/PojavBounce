0518200ca3c095a686c438e700052e9184e7eae5
Senk Ju <18741573+SenkJu@users.noreply.github.com>
2025-02-03 18:09:21 +0100

    chore: increase version to 0.26.1

--- src/main/kotlin/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -104,7 +104,7 @@ object LiquidBounce : EventListener {
      *
      * TODO: Replace this approach with full semantic versioning.
      */
-    const val IN_DEVELOPMENT = false
+    const val IN_DEVELOPMENT = true
 
     val isIntegrationTesting = !System.getenv("TENACC_TEST_PROVIDER").isNullOrBlank()
 

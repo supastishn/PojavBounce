ecbd27adbfe6665cac87e9e7ce92304489dc63c9
supastishn <omareeto2012@hotmail.com>
2026-01-21 08:02:30 +0300

    feat(android): Add LiquidBounce button to Title Screen
    - Adds centered "LiquidBounce" button below Multiplayer, above Options
    - Opens IntegrationMenuScreen with access to all native GUIs
    - Keeps existing Alts button in top-right corner

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/client/gui/screens/MixinTitleScreen.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/client/gui/screens/MixinTitleScreen.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
  *
- * Copyright (c) 2015 - 2025 CCBlueX
+ * Copyright (c) 2015 - 2026 CCBlueX
  *
  * LiquidBounce is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,23 +15,67 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
- *
  */
 package net.ccbluex.liquidbounce.injection.mixins.minecraft.client.gui.screens;
 
 import net.ccbluex.liquidbounce.features.feature.altmanager.AltManagerScreenButton;
+import net.ccbluex.liquidbounce.integration.ui.IntegrationMenuScreen;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.client.input.MouseButtonEvent;
+import net.minecraft.network.chat.Component;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TitleScreen.class)
-public class MixinTitleScreen {
+public abstract class MixinTitleScreen extends Screen {
+
+    protected MixinTitleScreen(Component title) {
+        super(title);
+    }
+
+    /**
+     * Add the LiquidBounce button to the title screen after init.
+     * Position: below Multiplayer, above Options (centered like other main buttons)
+     */
+    @Inject(method = "init", at = @At("TAIL"))
+    private void onInit(CallbackInfo ci) {
+        // Add LiquidBounce button - same width as main menu buttons (200), positioned between multiplayer and options
+        // Main buttons are typically at y = height/4 + 48 (singleplayer), +24 (multiplayer), etc.
+        // We insert our button with spacing that fits the menu flow
+        int buttonWidth = 200;
+        int buttonHeight = 20;
+        int centerX = this.width / 2 - buttonWidth / 2;
+
+        // Find a good Y position - typically after the main game buttons
+        // The vanilla buttons are around height/4 + 48, +72, +96 for the game modes
+        // Options is at height/4 + 96 + 24 = height/4 + 120
+        // We'll put our button at height/4 + 96 (after multiplayer area, before options)
+        int buttonY = this.height / 4 + 96;
+
+        Button liquidBounceButton = Button.builder(
+            Component.literal("LiquidBounce"),
+            button -> liquidbounce$openIntegrationMenu()
+        ).bounds(centerX, buttonY, buttonWidth, buttonHeight).build();
+
+        this.addRenderableWidget(liquidBounceButton);
+    }
+
+    /**
+     * Opens the Integration Menu screen.
+     */
+    @Unique
+    private void liquidbounce$openIntegrationMenu() {
+        Minecraft.getInstance().setScreen(new IntegrationMenuScreen());
+    }
 
     /**
      * Inject rendering of the Alt Manager button at the end of the render method.

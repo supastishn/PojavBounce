5efe917d076a9f77fab1613c0446afef6f5c858d
supastishn <omareeto2012@hotmail.com>
2026-01-23 19:23:45 +0300

    feat(combat): add BlockHit module for reactive blocking
    Automatically blocks when enemy is looking at you and within range.
    Uses facingEnemy raycast to detect when enemies are aiming at player.
    
    Settings:
    - Range: distance to detect enemies (min-max)
    - WallsRange: range through walls
    - BlockDuration: how long to hold block (ticks)
    - Cooldown: ticks between blocks

---
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleBlockHit.kt
@@ -0,0 +1,133 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2015 - 2026 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
+package net.ccbluex.liquidbounce.features.module.modules.combat
+
+import net.ccbluex.liquidbounce.event.tickHandler
+import net.ccbluex.liquidbounce.features.module.ClientModule
+import net.ccbluex.liquidbounce.features.module.ModuleCategories
+import net.ccbluex.liquidbounce.utils.aiming.utils.facingEnemy
+import net.ccbluex.liquidbounce.utils.combat.getEntitiesBoxInRange
+import net.ccbluex.liquidbounce.utils.combat.shouldBeAttacked
+import net.ccbluex.liquidbounce.utils.entity.rotation
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.ItemUseAnimation
+
+/**
+ * BlockHit module
+ *
+ * Automatically blocks when an enemy is looking at you and within range.
+ * Useful for defensive play - blocks incoming attacks reactively.
+ */
+object ModuleBlockHit : ClientModule("BlockHit", ModuleCategories.COMBAT) {
+
+    private val range by floatRange("Range", 3f..4f, 1f..8f)
+    private val wallsRange by float("WallsRange", 0f, 0f..8f)
+    private val blockDuration by intRange("BlockDuration", 1..3, 1..20, "ticks")
+    private val cooldown by int("Cooldown", 2, 0..20, "ticks")
+
+    private var blockTicks = 0
+    private var cooldownTicks = 0
+    private var isBlocking = false
+
+    @Suppress("unused")
+    private val tickHandler = tickHandler {
+        if (cooldownTicks > 0) {
+            cooldownTicks--
+            return@tickHandler
+        }
+
+        val blockHand = getBlockableHand()
+        if (blockHand == null) {
+            stopBlocking()
+            return@tickHandler
+        }
+
+        val enemies = world.getEntitiesBoxInRange(
+            player.eyePosition,
+            range.endInclusive.toDouble()
+        ) {
+            it != player && it.shouldBeAttacked()
+        }
+
+        val isInDanger = enemies.any { enemy ->
+            val distance = player.distanceTo(enemy).toDouble()
+            distance in range.start.toDouble()..range.endInclusive.toDouble() &&
+                facingEnemy(
+                    fromEntity = enemy,
+                    toEntity = player,
+                    rotation = enemy.rotation,
+                    range = range.endInclusive.toDouble(),
+                    wallsRange = wallsRange.toDouble()
+                )
+        }
+
+        if (isInDanger) {
+            if (!isBlocking) {
+                startBlocking(blockHand)
+                blockTicks = blockDuration.random()
+            }
+        }
+
+        if (isBlocking) {
+            blockTicks--
+            if (blockTicks <= 0) {
+                stopBlocking()
+                cooldownTicks = cooldown
+            }
+        }
+    }
+
+    private fun getBlockableHand(): InteractionHand? {
+        return when {
+            canBlock(player.mainHandItem) -> InteractionHand.MAIN_HAND
+            canBlock(player.offhandItem) -> InteractionHand.OFF_HAND
+            else -> null
+        }
+    }
+
+    private fun canBlock(itemStack: ItemStack): Boolean {
+        return itemStack.item?.getUseAnimation(itemStack) == ItemUseAnimation.BLOCK
+    }
+
+    private fun startBlocking(hand: InteractionHand) {
+        val itemStack = player.getItemInHand(hand)
+        if (itemStack.isEmpty || !itemStack.isItemEnabled(world.enabledFeatures())) {
+            return
+        }
+
+        val result = interaction.useItem(player, hand)
+        if (result.consumesAction()) {
+            isBlocking = true
+        }
+    }
+
+    private fun stopBlocking() {
+        if (isBlocking && player.isUsingItem) {
+            interaction.releaseUsingItem(player)
+        }
+        isBlocking = false
+    }
+
+    override fun onDisabled() {
+        stopBlocking()
+        blockTicks = 0
+        cooldownTicks = 0
+    }
+}
